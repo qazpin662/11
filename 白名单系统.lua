d37dfba1abd8e88c823fff36a741116a6d30a804
qazpin662 <3769797457@qq.com>
2024-12-04 17:23:57 +0800

    Update 白名单系统.lua

--- 白名单系统.lua
+++ 白名单系统.lua
@@ -1,8 +1,8 @@
 return {
 ["kxiaobai66"] = true,
-["uyd032"] = true,
-["520lhy1"] = true,
-["starsopy114514"] = true,
-["114514_CNnd"] = true,
+["uyd032"] = true
+["520lhy1"] = true
+["starsopy114514"] = true
+["114514_CNnd"] = true
 ["xxllll888"] = true
 }
